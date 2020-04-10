ca104ec3f796f899dd434d8d881ef7b67f6c709a
Praz314159 <praz.ramakrishna@gmail.com>
2020-04-10 17:07:56 -0400

    alpha = 6, beta = 2 figure

---
+++ generate_org_topologies.py
@@ -0,0 +1,12 @@
+''' 
+This collection of functions is meant to allow easy generation of common organization network topologies.
+Generating network topologies is important for a number of reasons. The most obvious one is that 
+organizations do not simply have fully connected topologies, in which everyone interacts with everyone else. 
+The truth is that there will be some degree of compartmentalization -- both vertical and horizontal -- in the 
+structure of the organization. 
+
+We will use networkX in order to build these topologies. However, 
+''' 
+
+
+
